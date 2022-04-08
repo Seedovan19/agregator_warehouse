d953087e5a5c6e694acc7af9dfc5716a40db4b95
Daniel <daniel.seidov@yandex.ru>
2022-04-08 21:44:41 +0300

    карта не отобразится на маленьком экране

--- agregator_front/src/App.js
+++ agregator_front/src/App.js
@@ -52,7 +52,7 @@ const App = () => {
           setType = {setType}
         />
       </Grid>
-      <Grid item xs={12} md={5}>
+      <Grid item xs={0} md={5}>
         <WarehouseMap
           warehouses = {warehouses}
           setChildClicked = {setChildClicked}
