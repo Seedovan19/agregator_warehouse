e7d3f995597fc8683b7015bbdf3950c9306408b1
Daniel <daniel.seidov@yandex.ru>
2022-04-22 04:02:33 +0300

    Ссылка, возврашающая к поиску

--- agregator_front/src/pages/WarehousePage/WarehousePage.jsx
+++ agregator_front/src/pages/WarehousePage/WarehousePage.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import Slideshow from '../../components/Slideshow/Slideshow'
 import Header from '../../components/Header/Header'
-import { Grid, CircularProgress, Card } from '@material-ui/core';
+import { Grid, Link, CircularProgress, Card } from '@material-ui/core';
 import useStyles from './styles'
 import { getWarehouseImagesData } from '../../api'
-
+import { useNavigate } from "react-router-dom";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 
 const WarehousePage = () => {
@@ -13,6 +14,7 @@ const WarehousePage = () => {
     const [ warehouse, setWarehouse ] = useState(null);
     const [ warehouseImages, setWarehouseImages ] = useState(null);
     const classes = useStyles();
+    const navigate = useNavigate();
 
 
     useEffect(() => {
@@ -24,12 +26,39 @@ const WarehousePage = () => {
         .then(data => setWarehouseImages(data))
         
     }, [id]);
-    
+
+    const handleLinkClick = (event) => {
+        let path = `/`; 
+        navigate(path);
+    }
+
     return (
     <div>
         <Header/>
-        <div container className={classes.page_content}>
-            <div className={classes.slideshow_div}>
+        <Grid container className={classes.page_content}>
+        <Grid item md={10}>
+        <div className={classes.go_back_link} onClick={handleLinkClick}>
+            <ArrowBackIosIcon
+            sx={{
+                position: 'relative',
+                top: 2,
+                height: '15px',
+            }}
+            />
+            <Link
+            component="button" 
+            underline="always"
+            className={classes.go_back_link_text}
+            >
+                Вернуться к поиску
+            </Link>
+        </div>
+        </Grid>
+        </Grid>
+        <Grid container className={classes.page_content}>
+
+        <Grid item md ={7} sm={12}>
+            <div>
             {warehouse ? (
                 <>
                 <Slideshow warehouseImages={warehouseImages}/>
@@ -44,10 +73,13 @@ const WarehousePage = () => {
                 </>
             )}
             </div>
+        </Grid>
+        <Grid item md ={3} sm={12}>
             <div className={classes.apply_card_div}>
                 <Card variant="outlined" className={classes.apply_card}/>
             </div>
-        </div>
+        </Grid>
+        </Grid>
     </div>
   )
 }
--- agregator_front/src/pages/WarehousePage/styles.js
+++ agregator_front/src/pages/WarehousePage/styles.js
@@ -5,13 +5,41 @@ export default makeStyles((theme) => ({
         display: 'flex',
         justifyContent: 'center',
         margin: '0 auto',
+        position: 'relative',
     },
-    slideshow_div: {
+
+    go_back_link : {
+        width: '200px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        textAlign: 'center',
+        marginBottom: '10px',
+        
+    },
+
+    go_back_link_text: {
+        fontFamily: 'Lato-Regular',
+        fontSize: '17px',
+        color: 'black',
+        textDecoration: 'underline',
+        textDecorationColor: '#8046F1',
+        textDecorationThickness: '2px',
+        textUnderlinePosition: 'under',
+        position: 'relative',
 
     },
-    apply_card_div :{},
+
+    apply_card_div :{
+        position: '-webkit-sticky',
+        position: 'sticky',
+        top: '1.5rem',
+        left: 0,
+    },
+
     apply_card: {
-        width: '100%',
-        height: '400px',
+        minWidth: '320px',
+        height: '200px',
+        marginLeft: '2rem',
     }
 }))
