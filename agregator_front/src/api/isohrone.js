44a057e5550ebdf7b5f0f05704147ac94684e25a
Daniel <daniel.seidov@yandex.ru>
2022-05-31 17:57:34 +0300

    api для изохрона

---
+++ agregator_front/src/api/isohrone.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+
+export const getIsochrone = async (values) => {
+    const article = { 
+        ...(values.vehicle ? { vehicle: values.vehicle } : {}),
+    };
+    try { 
+        const { data } = await axios.post(`http://127.0.0.1:5000/isochrone`, article);
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+};
+// 1) Выбор средства передвижения (велосипед, пешеход, грузовик) 
